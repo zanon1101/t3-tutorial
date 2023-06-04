fec7fa37ce24b705d633560ef0dcaf4918f5bf9b
Elliott <elliott@myconnect.com.au>
2023-06-04 19:29:07 +1000

    toast post, use enter key for posting, error messaging if post is not an emoji

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,9 @@ import { type RouterOutputs, api } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -24,14 +25,23 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
-    }
+    },
+
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post you must enter an emoji!");
+      }
+    },
   });
 
   if (!user) return null;
 
   return (
     <div className="flex gap-3 w-full">
-      <Image 
+      <Image
         src={user.profileImageUrl} 
         alt="Profile image"
         className="h-14 w-14 rounded-full"
@@ -45,8 +55,27 @@ const CreatePostWizard = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              if (input !== "") {
+                mutate({ content: input });
+              }
+            }
+          }
+        }
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting &&
+        <button onClick={() => mutate({ content: input })}>
+          Post
+        </button>
+      }
+
+      {isPosting && 
+        <div className="flex justify-center items-center">
+          <LoadingSpinner size={20}/>
+        </div>
+      }
     </div>
   );
 };
